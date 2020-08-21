521e989c02e8ebc39546861d2ec6e2c0759d9144
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-21 14:19:09 +0000

    init

---
+++ README.md
@@ -0,0 +1,63 @@
+I am Groot [I am Groot ](https://github.com/facebook/create-react-app).
+
+## I am Groot 
+
+I am Groot 
+
+### `I am Groot `
+
+I am Groot <br />I am Groot [I am Groot ](http://localhost:3000)I am Groot 
+
+I am Groot <br />I am Groot 
+
+### `I am Groot `
+
+I am Groot <br />I am Groot [I am Groot ](https://facebook.github.io/create-react-app/docs/running-tests)I am Groot 
+
+### `I am Groot `
+
+I am Groot `I am Groot `I am Groot <br />I am Groot 
+
+I am Groot <br />I am Groot 
+
+I am Groot [I am Groot ](https://facebook.github.io/create-react-app/docs/deployment)I am Groot 
+
+### `I am Groot `
+
+**I am Groot `I am Groot `I am Groot **
+
+I am Groot `I am Groot `I am Groot 
+
+I am Groot `I am Groot `I am Groot 
+
+I am Groot `I am Groot `I am Groot 
+
+## I am Groot 
+
+I am Groot [I am Groot ](https://facebook.github.io/create-react-app/docs/getting-started).
+
+I am Groot [I am Groot ](https://reactjs.org/).
+
+### I am Groot 
+
+I am Groot [I am Groot ](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### I am Groot 
+
+I am Groot [I am Groot ](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### I am Groot 
+
+I am Groot [I am Groot ](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### I am Groot 
+
+I am Groot [I am Groot ](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### I am Groot 
+
+I am Groot [I am Groot ](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `I am Groot `I am Groot 
+
+I am Groot [I am Groot ](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
---
+++ index.js
@@ -0,0 +1,63 @@
+const { readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
+const unified = require('unified');
+const parse = require('remark-parse');
+const stringify = require('remark-stringify');
+const visit = require('unist-util-visit');
+const { default: Axios } = require('axios');
+const simpleGit = require('simple-git');
+const git = simpleGit();
+
+const toAst = (markdown) => {
+  return unified().use(parse).parse(markdown);
+};
+
+const toMarkdown = (ast) => {
+  return unified().use(stringify).stringify(ast);
+};
+
+const README = 'README.md' || 'readme.md';
+const mainDir = '.';
+const readme = readFileSync(join(mainDir, README), { encoding: 'utf8' });
+const readmeAST = toAst(readme);
+let LANG = 'I am Groot ';
+
+visit(readmeAST, (node) => {
+  if (
+    (node.type === 'text' || node.type === 'inlineCode' || node.type === 'code') &&
+    node.value !== '.' &&
+    node.value !== ' '
+  ) {
+    node.value = LANG;
+  }
+});
+
+writeFileSync(join(mainDir, README), toMarkdown(readmeAST), 'utf8');
+
+async function commit() {
+  const mail = await Axios.get(`https://api.github.com/users/${process.env.GITHUB_ACTOR}/events`);
+  let email;
+  let author;
+  await mail.data.forEach((data) => {
+    if (data.type === 'PushEvent') {
+      email = data.payload.commits[0].author.email;
+    } else {
+      author = 'github-actions[bot]';
+      email = '41898282+github-actions[bot]@users.noreply.github.com';
+    }
+  });
+
+  actor = author ? author : process.env.GITHUB_ACTOR;
+
+  await git.add('./*');
+  await git.addConfig('user.name', actor);
+  await git.addConfig('user.email', email);
+  await git.commit(LANG);
+  // await git.push();
+}
+
+try {
+  commit();
+} catch (error) {
+  throw new Error(error);
+}
